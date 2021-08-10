72e7c589fc2d6d8d2a734ff6bd8f5169c087a616
OtavioMalta <83037014+OtavioMalta@users.noreply.github.com>
2021-08-10 13:10:09 -0300

    ADD

---
+++ ERP/estoque/script.js
@@ -0,0 +1,11 @@
+function visualizacao(){
+    document.getElementById("pagina-visualizacao").style.display="block";
+    document.getElementById("pagina-formulario").style.display="none";
+}
+
+function formulario(){
+    document.getElementById("pagina-visualizacao").style.display="none";
+    document.getElementById("pagina-formulario").style.display="block";
+}
+
+
